f175e8e3403d801f0d7267f1678cdfeac4aee479
Sangeeta Saha <sangeeta317@gmail.com>
2020-11-16 19:43:52 +0530

    Third commit

--- ParkingLot.py
+++ ParkingLot.py
@@ -21,20 +21,26 @@ class ParkingLot:
             print("Sorry, Parking lot is full")
 
        
-    def printStatus(self):           
-        print ("Slot No. Registration No Colour") 
+    def printStatus(self): 
+        header1 = "{0:<10}".format("Slot No.")  + "{0:<25}".format("Registration No.") + "{0:<15}".format("Colour")
+        print (header1) 
+        header2 = "-" * 50
+        print (header2)
         if self.isParkingEmpty:
             print("Parking Lot is vacant. No car details to display")
         else:
              for i in range(len(self.spaces)):
                 if self.spaces[i] is not None:
-                    print(i+1, self.spaces[i].reg_no, self.spaces[i].colour)
+                    detail =  "{0:<10}".format(i+1)  + "{0:<25}".format( self.spaces[i].reg_no) + "{0:<15}".format(self.spaces[i].colour)                    
+                    print(detail)
 
-    def leaveCar(self,slot):        
-        if slot > len(self.spaces):
+    def leaveCar(self,slot):
+        
+        if slot <= 0 or slot > len(self.spaces):
             print("Please enter a valid Slot number")
         elif self.spaces[slot - 1] is not None:
             self.spaces[slot - 1] = None
+            print("Slot number ", slot, " is free")
             if self.isParkingFull:
                 self.isParkingFull = False
         else:
@@ -75,15 +81,14 @@ class ParkingLot:
         else:
             print(op)
 
-    def printSlotNumberForRegNo(self,reg_no):
-        
+    def printSlotNumberForRegNo(self,reg_no):        
         if self.isParkingEmpty:
             print("Parking Lot is empty.")
         else:
             for i in range(len(self.spaces)):
                 if self.spaces[i] is not None:
                     if self.spaces[i].reg_no == reg_no:
-                        print(i)
+                        print(i + 1)
                         return
             print("Not found")
         
@@ -91,85 +96,133 @@ class ParkingLot:
 class Car:
     def __init__(self, reg_no, colour):
         self.reg_no = reg_no
-        self.colour = colour       
+        self.colour = colour
 
-def readInputPrompt():
+def createParkingLot(n=5):
+    ParkingLot1 = ParkingLot(int(n))
+    return ParkingLot1
+
+def listValidCommands():
+    print("List of valid commands")
+    print(" 1> create_parking_lot no" )
+    print(" 2> park registration_no color")
+    print(" 3> status")
+    print(" 4> leave slot_no")
+    print(" 5> registration_numbers_for_cars_with_colour colour ")
+    print(" 6> slot_numbers_for_cars_with_colour colour ")
+    print(" 7> slot_number_for_registration_number registration_no")
+
+
+def executeCommands(command):
+    global PL
+    global parkingLotCreated 
+    arr = command.split()   
+    if arr[0] == "create_parking_lot":        
+        if len(arr) < 2:
+            if not parkingLotCreated:
+                PL = createParkingLot()
+                parkingLotCreated = True
+                print("As no.of slots was not specified, created a parking lot with default 5 slots ")
+            else:
+                print("Parking Lot already created")
+        else:
+            if not parkingLotCreated:
+                PL = createParkingLot(arr[1])
+                parkingLotCreated = True
+                print("Created a parking lot with ", arr[1], "slots")
+            else:
+                print("Parking Lot already created")        
+
+    elif arr[0] == "park":                       
+        if PL:
+            if len(arr) == 1:
+                print("Enter the command again specifying the car registration number and color")
+                return
+            elif len(arr) == 2:
+                print("Enter the command again specifying the car color")
+                return
+            elif len(arr) == 3:
+                colour = arr[2].upper()
+                car = Car(arr[1],colour)  
+                PL.parkCar(car)
+        else:
+            print("Parking Lot has not yet been created. Cannot issue parking ticket")
+        
+    elif arr[0] == "status": 
+        if PL:
+            PL.printStatus()
+        else:
+            print("ParkingLot has not yet been created. Cannot print Status")    
+    
+    elif arr[0] == "leave": 
+        if PL:
+            PL.leaveCar(int(arr[1]))            
+        else:
+            print("ParkingLot has not yet been created.") 
+    
+
+    elif arr[0] == "slot_numbers_for_cars_with_colour": 
+        if PL:
+            PL.printSlotNumbersForColour(arr[1].upper())            
+        else:
+            print("ParkingLot has not yet been created.")
+
+
+    elif arr[0] == "registration_numbers_for_cars_with_colour":
+        if PL:
+            PL.printRegNumbersForColour(arr[1].upper())            
+        else:
+            print("ParkingLot has not yet been created.")
+
+
+    elif arr[0] == "slot_number_for_registration_number":
+        if PL:
+            PL.printSlotNumberForRegNo(arr[1])            
+        else:
+            print("ParkingLot has not yet been created.")
+    else:
+        print("Not a valid command") 
+       
+# For interactive command prompt based shell where commands can be typed in
+def readInputPrompt():    
     print("Reading from command prompt")
-    print("Start entering inputs")
-    PL = None
+    listValidCommands()
+    print("Start entering inputs")    
     while(True):
         ip_command = input()
         if ip_command == "exit":
             break           
         else:           
-            arr = ip_command.split()   
-            if arr[0] == "create_parking_lot":  
-                PL = createParkingLot(arr[1])
-                print("Created a parking lot with ", arr[1], "slots")
-                              
-            elif arr[0] == "park":                
-                if PL:
-                    colour = arr[2].upper()
-                    car = Car(arr[1],colour)  
-                    print(car.reg_no,car.colour)                  
-                    PL.parkCar(car)
-                                        
-                else:
-                    print("ParkingLot has not yet been created. Cannot issue parking ticket")
-            
-            elif arr[0] == "status": 
-                if PL:
-                    PL.printStatus()
-                else:
-                    print("ParkingLot has not yet been created. Cannot print Status")
-           
-            
-            elif arr[0] == "leave": 
-                if PL:
-                    PL.leaveCar(int(arr[1]))
-                    
-                else:
-                    print("ParkingLot has not yet been created.") 
-            
-        
-            elif arr[0] == "slot_numbers_for_cars_with_colour": 
-                if PL:
-                    PL.printSlotNumbersForColour(arr[1].upper())
-                     
-                else:
-                    print("ParkingLot has not yet been created.")
-            elif arr[0] == "registration_numbers_for_cars_with_colour":
-                if PL:
-                    PL.printRegNumbersForColour(arr[1].upper())
-                    
-                else:
-                    print("ParkingLot has not yet been created.")
-            elif arr[0] == "slot_number_for_registration_number":
-                if PL:
-                    PL.printSlotNumberForRegNo(arr[1])
-                     
-                else:
-                    print("ParkingLot has not yet been created.")
-
-            else:
-                print("Not a valid command")            
+            executeCommands (ip_command)  
 
 
-
+# For accepting a filename as a parameter at the command prompt and read the commands from that file
 def readInputFile():
+    
     print("Reading from file")
     print("Enter the name of the input file")
+    filename = input()
+    file1 = open(filename,"r")    
+    for line in file1:
+        executeCommands(line)
 
-def createParkingLot(n):
-    ParkingLot1 = ParkingLot(int(n))
-    return ParkingLot1
+
+def main():
+    print ("Enter 1 if you want to enter inputs using command prompt")
+    print ("Enter 2 if you want to enter inputs using a file")
+    #global variable PL to denote ParkingLot object to enusre that all input commands are run for the same  ParkingLot object
+    global PL
+    PL = None
+    #global variable parkingLotCreated to ensure that duplicate ParkingLot objects are not created.
+    global parkingLotCreated
+    parkingLotCreated = False
+
+    typeOfInput = int(input())
+    if (typeOfInput) == 1:
+        readInputPrompt()
+    if (typeOfInput) == 2:
+        readInputFile()
 
 
-
-print ("Enter 1 if you want to enter inputs using command prompt")
-print ("Enter 2 if you want to enter inputs using a file")
-typeOfInput = int(input())
-if (typeOfInput) == 1:
-    readInputPrompt()
-if (typeOfInput) == 2:
-    readInputFile()
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This application implements an automated ticketing system for a parking lot.
 On entering the car registration number and the colour, next free parking slot is allocated.
 
-#### Installation
+### Installation
 
 You can download the repo or clone to your local machine
 
@@ -12,5 +12,5 @@ You can download the repo or clone to your local machine
 - step 2
   cd project-sangeeta-saha-au8
 - step 3
-  Make sure your machine has python 3.8 installed
-  at the command-prompt > python ParkingLot.py
+  Make sure your machine has python 3.8 installed.
+  At the command-prompt > python ParkingLot.py
